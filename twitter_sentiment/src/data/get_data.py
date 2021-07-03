b342aa2fad75453a71697a82c040c1e39ef81aa9
Jahnic <jahnic.beck-joseph@mail.mcgill.ca>
2021-07-03 22:24:15 +0200

    add wait on rate limit functionality to tweepy API

--- twitter_sentiment/src/data/get_data.py
+++ twitter_sentiment/src/data/get_data.py
@@ -7,12 +7,19 @@ with open("../data/raw/twitter_creds.json") as f:
 
 auth = tweepy.OAuthHandler(twitter_credentials['consumer_key'], twitter_credentials['consumer_secret'])
 auth.set_access_token(twitter_credentials['access_token'], twitter_credentials['access_token_secret'])
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True,
+                 wait_on_rate_limit_notify=True)
+
 
 def get_recent_tweets(user, n=20):
     all_tweets = []
     # q -> defines query for tweets
-    for tweet in Cursor(api.search, q=f"@{user}", count=n, tweet_mode="extended").items():
+    # extended mode for new 280 character tweet cap
+    for tweet in Cursor(api.search,
+                        q=f"@{user}",
+                        lang='en',
+                        count=n,
+                        tweet_mode="extended").items():
         # append text and link of tweet
         all_tweets.append((tweet.full_text, f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"))
     return all_tweets
